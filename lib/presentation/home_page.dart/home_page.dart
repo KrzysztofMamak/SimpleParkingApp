0cd2fe994066bde2750d1717b367820317f5f974
Krzysztof Mamak <mamako@onet.pl>
2021-03-01 18:47:22 +0100

    showing map on the HomePage

--- lib/presentation/home_page.dart/home_page.dart
+++ lib/presentation/home_page.dart/home_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _setStatusBarAsTransparent();
     return Scaffold(
-      body: Container(
-        color: Colors.blue,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * (2 / 5),
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(50.317852, 18.667160),
+                zoom: 16.0,
+              ),
+              zoomGesturesEnabled: true,
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          Expanded(
+            child: Container(),
+          ),
+        ],
       ),
     );
   }
